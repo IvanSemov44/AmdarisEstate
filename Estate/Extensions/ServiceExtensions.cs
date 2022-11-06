8e29afeffb084252307a439cbd55206ee0faa7cb
Иван Семов <ivan.semov.44@gmail.com>
2022-11-06 20:25:30 +0200

    Add project files.

---
+++ Estate/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace Estate.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCORS(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", bulder =>
+                bulder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    }
+}
---
+++ Estate/Program.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.AspNetCore.HttpOverrides;
+
+using NLog;
+
+using Estate.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+// Add services to the container.
+builder.Services.ConfigureCORS();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
