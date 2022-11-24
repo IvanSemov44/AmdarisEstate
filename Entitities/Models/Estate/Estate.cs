8111d35ec148ff65e244d869247c71abaf4dd6ad
Иван Семов <ivan.semov.44@gmail.com>
2022-11-24 16:11:43 +0200

    Add Estate Model

---
+++ Entitities/Models/Estate/Estate.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Entities.Models.Estate
+{
+    public class Estate
+    {
+        public string? Ciry { get; set; }
+
+        public string? Neighborhood { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Currency { get; set; }
+
+        public string? Type { get; set; }
+
+        public int Floоr { get; set; }
+
+        public int Rooms { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Extras { get; set; }
+
+        public string? YearOfCreation { get; set; }
+
+        public bool Sell { get; set; }
+
+        public DateTime? Created { get; set; }
+
+        public DateTime? Changed { get; set; }
+
+        
+
+    }
+}
---
+++ Entitities/Models/Estate/EstateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models.Estate
+{
+    public enum EstateType
+    {
+        apartment = 2,
+        house = 4,
+        land = 8,
+        Bis
+    }
+}
