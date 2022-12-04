19c06fadc5bdfa2fa5aa954b1dc3e22b7dfbb8f2
Иван Семов <ivan.semov.44@gmail.com>
2022-12-04 13:22:32 +0200

    Add CreateImageTests

---
+++ TestProject1/HandlersTests/ImageTest/CreateImageTests.cs
@@ -0,0 +1,43 @@
+﻿namespace IvanRealEstate.Test.HandlersTests.ImageTest
+{
+    using Moq;
+    using AutoMapper;
+
+    using IvanRealEstate.Contracts;
+    using IvanRealEstate.Shared.DataTransferObject.Image;
+    using IvanRealEstate.Application.Commands.ImageCommads;
+    using IvanRealEstate.Application.Handlers.ImageHandlers;
+
+    public class CreateImageTests
+    {
+        private readonly Guid _estateId;
+        private readonly IMapper _mapper;
+        private readonly Mock<IRepositoryManager> _mockRepo;
+        private readonly ImageForCreationDto _imageForCreationDto;
+
+        public CreateImageTests()
+        {
+            _estateId = Guid.NewGuid();
+            _mapper = MapperConfig.Configuration();
+            _mockRepo = MockImageRepository.ImageRepositoryForTest();
+
+            _imageForCreationDto = new ImageForCreationDto
+            {
+                ImageUrl = "https://www.images.com"
+            };
+        }
+
+        [Fact]
+        public async Task Valid_CreateImageHandler_Test()
+        {
+            var handler = new CreateImageHandler(_mockRepo.Object, _mapper);
+            var result = await handler
+                .Handle(new CreateImageCommand(
+                    _estateId, _imageForCreationDto, false), CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<ImageDto>(result);
+            Assert.True("https://www.images.com" == result.ImageUrl);
+        }
+    }
+}
