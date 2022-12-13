355beb1b4ccb8561eeaf41dabc89bc09987a9333
Иван Семов <ivan.semov.44@gmail.com>
2022-12-13 17:16:42 +0200

    Add DeleteCurrencyHandlerTest

--- Application/Handlers/CurrencyHandlers/DeleteCurrencyHandler.cs
+++ Application/Handlers/CurrencyHandlers/DeleteCurrencyHandler.cs
@@ -5,7 +5,7 @@
     using IvanRealEstate.Contracts;
     using IvanRealEstate.Application.Commands.CurrencyCommands;
 
-    internal sealed class DeleteCurrencyHandler : IRequestHandler<DeleteCurrencyCommand, Unit>
+    public sealed class DeleteCurrencyHandler : IRequestHandler<DeleteCurrencyCommand, Unit>
     {
 
         private readonly IRepositoryManager _repositoryManager;
---
+++ TestProject1/HandlersTests/CurrencyTest/DeleteCurrencyHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿namespace IvanRealEstate.Test.HandlersTests.CurrencyTest
+{
+    using Moq;
+
+    using IvanRealEstate.Contracts;
+    using IvanRealEstate.Application.Handlers.CurrencyHandlers;
+    using IvanRealEstate.Application.Commands.CurrencyCommands;
+
+    public class DeleteCurrencyHandlerTest
+    {
+        private readonly Guid _currencyId;
+        private readonly Mock<IRepositoryManager> _mockRepo;
+
+        public DeleteCurrencyHandlerTest()
+        {
+            _currencyId = Guid.Parse("4bb67d01-13cb-47d5-d499-08dad1453af0");
+            _mockRepo = MockCurrencyRepository.CurrencyRepositoryForTest(_currencyId);
+        }
+
+        [Fact]
+        public async Task Valid_DeleteCurrencyHandlerTest()
+        {
+            var handler = new DeleteCurrencyHandler(_mockRepo.Object);
+            await handler.Handle(new DeleteCurrencyCommand(_currencyId, TrackChanges: false),
+                CancellationToken.None);
+
+            _mockRepo.Verify();
+        }
+    }
+}
