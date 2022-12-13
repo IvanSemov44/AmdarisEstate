ee5e999462da2bf94172ff12e3f8f337247fe6c5
Иван Семов <ivan.semov.44@gmail.com>
2022-12-13 13:39:41 +0200

    Add CreateCountryHandlerTest

--- TestProject1/HandlersTests/CountryTests/CreateCountryHandlerTest.cs
+++ TestProject1/HandlersTests/CountryTests/CreateCountryHandlerTest.cs
@@ -1,9 +1,40 @@
 ﻿namespace IvanRealEstate.Test.HandlersTests.CountryTests
 {
     using Moq;
+    using AutoMapper;
+
+    using IvanRealEstate.Contracts;
+    using IvanRealEstate.Shared.DataTransferObject.Country;
+    using IvanRealEstate.Application.Commands.CountryCommands;
+    using IvanRealEstate.Application.Handlers.CountryHandlers;
 
     public class CreateCountryHandlerTest
     {
+        private readonly IMapper _mapper;
+        private readonly Mock<IRepositoryManager> _mockRepo;
+        private readonly CountryForCreationDto _countryForCreationDto;
 
+        public CreateCountryHandlerTest()
+        {
+            _mapper = MapperConfig.Configuration();
+            _mockRepo = MockCountryRepository.GetCountryRespository();
+
+            _countryForCreationDto = new CountryForCreationDto
+            {
+                CountryName = "Moldova"
+            };
+        }
+
+        [Fact]
+        public async Task Valid_CreateCountryHandlerTest()
+        {
+            var handler = new CreateCountryHandler(_mockRepo.Object, _mapper);
+            var result = await handler
+                .Handle(new CreateCountryCommand(_countryForCreationDto),CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<CountryDto>(result);
+            Assert.Contains("Moldova", result.CountryName);
+        }
     }
 }
