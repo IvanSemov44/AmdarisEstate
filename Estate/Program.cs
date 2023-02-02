e4ff7ac566e79accf22f5f9b146d40f3b6524280
Иван Семов <ivan.semov.44@gmail.com>
2023-02-02 12:40:15 +0200

    ok

--- Estate/Program.cs
+++ Estate/Program.cs
@@ -19,6 +19,9 @@ builder.Services.ConfigureIdentity();
 builder.Services.ConfigureJWT(builder.Configuration);
 builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
+
+
+
 builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddAutoMapper(typeof(Program));
